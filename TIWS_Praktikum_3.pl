15586fe66758e1776a3a1468b28feeeb61bb7a19
VShader <t.weitz66@web.de>
2013-11-24 14:21:16 +0100

    clear code

--- TIWS_Praktikum_3.pl
+++ TIWS_Praktikum_3.pl
@@ -21,7 +21,6 @@ postorder([X|[Lb,Rb]],Ys) :- postorder(Lb,H1s), postorder(Rb,H2s), append(H1s,H2
 tiefe([],[]).
 tiefe([X|[Lb,Rb]],Ys) :- tiefe(Lb,H1s), tiefe(Rb,H2s), append(H1s,[X],H3s), append(H3s,H2s,Ys).
 
-
 %roots(Xbs,Ys)
 roots([],[]) :-!.
 roots([Xb|Ybs],Es) :- root(Xb,X1), roots(Ybs,H1s), append([X1],H1s,Es).
@@ -29,23 +28,6 @@ roots([Xb|Ybs],Es) :- root(Xb,X1), roots(Ybs,H1s), append([X1],H1s,Es).
 %Q roots([[1, [2,[],[]] ,[]],[3, [4,[],[]] ,[]],[23, [32,[],[]] ,[]]],Zs).
 %Q roots([[5, [3,[2,[],[]],[4,[],[]]] ,[7,[6,[],[]],[8,[],[]]]],[5, [3,[2,[],[]],[4,[],[]]] ,[7,[6,[],[]],[8,[],[]]]],[5, [3,[2,[],[]],[4,[],[]]] ,[7,[6,[],[]],[8,[],[]]]]],Zs).
 
-%Hilfskonstrukte
-%bintree(Xb) : Xb ist ein Bin‰rbaum
-bintree([]) :-!.
-bintree([X|[Lb,Rb]]) :- bintree(Lb), bintree(Rb).
-%bintree(list(X,list(R))
-
-%root(Xb,X) : Xb ist ein Baum, X ist die Wurzel des Baumes Xb.
-root([X|[Lb,Rb]],X).
-
-%left(Xb,Lb) : Xb ist ein Baum, Lb ist der linke Teilbaum von Xb.
-left([X|[Lb,Rb]],Lb).
-
-%right(Xb,Rb) : Xb ist ein Baum, Rb ist der rechte Teilbaum von Xb.
-right([X|[Lb,Rb]],Rb).
-
-dosort([],[]).
-swap(X,Y,Rs)  .
 
 
 %Sonderufgabe (auﬂer Konkrruenz)
@@ -59,7 +41,6 @@ tof(N,Ys) :- move(N,1,3,2,Ys).
 
 %move(N,A,B,C,Ys)
 move(0,A,B,C,[]) :-!.
-%move(1,A,B,C,[von, A, nach, B]) :-!.
 move(N,A,B,C,Ys) :- Hn is N-1, move(Hn,A,C,B,H1s), move(Hn,C,B,A,H2s), append(H1s,[von, A, nach, B],H3s), append(H3s,H2s,Ys).
 
 
@@ -80,3 +61,20 @@ getPivot([X|Xs],X,Xs).
 %rightlist([3,2,4],3,[4]).
 %leftlist([2,3],2,[2]).
 %rightlist([2,3],2,[3]).
+
+
+
+%Hilfskonstrukte
+%bintree(Xb) : Xb ist ein Bin‰rbaum
+bintree([]) :-!.
+bintree([X|[Lb,Rb]]) :- bintree(Lb), bintree(Rb).
+%bintree(list(X,list(R))
+
+%root(Xb,X) : Xb ist ein Baum, X ist die Wurzel des Baumes Xb.
+root([X|[Lb,Rb]],X).
+
+%left(Xb,Lb) : Xb ist ein Baum, Lb ist der linke Teilbaum von Xb.
+left([X|[Lb,Rb]],Lb).
+
+%right(Xb,Rb) : Xb ist ein Baum, Rb ist der rechte Teilbaum von Xb.
+right([X|[Lb,Rb]],Rb).
