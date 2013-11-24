79a9a65ff1fb910a97bbe315f326cc85b1e2d246
VShader <t.weitz66@web.de>
2013-11-24 13:29:22 +0100

    second approach for tree and hanoi

--- TIWS_Praktikum_3.pl
+++ TIWS_Praktikum_3.pl
@@ -6,12 +6,12 @@
 präorder([],[]).
 präorder([X|[Lb,Rb]],Ys) :- präorder(Lb,H1s), präorder(Rb,H2s), append([X],H1s,H3s), append(H3s,H2s,Ys).
 
-%Q präorder([1, [2,[],[]] ,[]],Z).
-%Q präorder([5, [3,[],[]] ,[7,[],[]]],Z).
-%Q präorder([5, [3,[2,[],[]],[4,[],[]]] ,[7,[6,[],[]],[8,[],[]]]],Z).
-%                 5
-%              3     7
-%             2 4   6  8
+%Q präorder([b, [a,[],[]] ,[]],Z).
+%Q präorder([b, [a,[],[]] ,[c,[],[]]],Z).
+%Q präorder([d, [b,[a,[],[]],[c,[],[]]] ,[f,[e,[],[]],[g,[],[]]]],Z).
+%                 d
+%              b     f
+%             a c   e  g
 
 %postorder(Xb,Ys)
 postorder([],[]).
@@ -21,31 +21,12 @@ postorder([X|[Lb,Rb]],Ys) :- postorder(Lb,H1s), postorder(Rb,H2s), append(H1s,H2
 tiefe([],[]).
 tiefe([X|[Lb,Rb]],Ys) :- tiefe(Lb,H1s), tiefe(Rb,H2s), append(H1s,[X],H3s), append(H3s,H2s,Ys).
 
-
 %roots(Xbs,Ys)
 roots([],[]) :-!.
 roots([Xb|Ybs],Es) :- root(Xb,X1), roots(Ybs,H1s), append([X1],H1s,Es).
 
-%Q roots([[1, [2,[],[]] ,[]],[3, [4,[],[]] ,[]],[23, [32,[],[]] ,[]]],Zs).
-%Q roots([[5, [3,[2,[],[]],[4,[],[]]] ,[7,[6,[],[]],[8,[],[]]]],[5, [3,[2,[],[]],[4,[],[]]] ,[7,[6,[],[]],[8,[],[]]]],[5, [3,[2,[],[]],[4,[],[]]] ,[7,[6,[],[]],[8,[],[]]]]],Zs).
-
-%Hilfskonstrukte
-%bintree(Xb) : Xb ist ein Binärbaum
-bintree([]) :-!.
-bintree([X|[Lb,Rb]]) :- bintree(Lb), bintree(Rb).
-%bintree(list(X,list(R))
-
-%root(Xb,X) : Xb ist ein Baum, X ist die Wurzel des Baumes Xb.
-root([X|[Lb,Rb]],X).
-
-%left(Xb,Lb) : Xb ist ein Baum, Lb ist der linke Teilbaum von Xb.
-left([X|[Lb,Rb]],Lb).
-
-%right(Xb,Rb) : Xb ist ein Baum, Rb ist der rechte Teilbaum von Xb.
-right([X|[Lb,Rb]],Rb).
-
-dosort([],[]).
-swap(X,Y,Rs)  .
+%Q roots([[b,[a,[],[]],[]],  [f,[e,[],[]],[]]],Zs).
+%Q roots([[d, [b,[a,[],[]],[c,[],[]]] ,[f,[e,[],[]],[g,[],[]]]],  [d, [b,[a,[],[]],[c,[],[]]] ,[f,[e,[],[]],[g,[],[]]]]],Zs).
 
 
 %Sonderufgabe (außer Konkrruenz)
@@ -55,11 +36,10 @@ swap(X,Y,Rs)  .
 
 %Aufgabe 2: (Türme von Hanoi)
 %tof(X,Ys).
-tof(N,Ys) :- move(N,1,3,2,Ys).
+tof(N,Ys) :- move(N,a,b,c,Ys).
 
 %move(N,A,B,C,Ys)
-move(0,A,B,C,[]) :-!.
-%move(1,A,B,C,[von, A, nach, B]) :-!.
+move(1,A,B,C,[von, A, nach, B]) :-!.
 move(N,A,B,C,Ys) :- Hn is N-1, move(Hn,A,C,B,H1s), move(Hn,C,B,A,H2s), append(H1s,[von, A, nach, B],H3s), append(H3s,H2s,Ys).
 
 
@@ -80,3 +60,21 @@ getPivot([X|Xs],X,Xs).
 %rightlist([3,2,4],3,[4]).
 %leftlist([2,3],2,[2]).
 %rightlist([2,3],2,[3]).
+
+
+
+
+%Hilfskonstrukte
+%bintree(Xb) : Xb ist ein Binärbaum
+bintree([]) :-!.
+bintree([X|[Lb,Rb]]) :- bintree(Lb), bintree(Rb).
+%bintree(list(X,list(R))
+
+%root(Xb,X) : Xb ist ein Baum, X ist die Wurzel des Baumes Xb.
+root([X|[Lb,Rb]],X).
+
+%left(Xb,Lb) : Xb ist ein Baum, Lb ist der linke Teilbaum von Xb.
+left([X|[Lb,Rb]],Lb).
+
+%right(Xb,Rb) : Xb ist ein Baum, Rb ist der rechte Teilbaum von Xb.
+right([X|[Lb,Rb]],Rb).
