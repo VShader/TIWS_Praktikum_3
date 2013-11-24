a2eec3343e4ec4c0ee093b9d148c70953c1b92db
VShader <t.weitz66@web.de>
2013-11-24 13:15:29 +0100

    add Towers of Hanoi and roots()

--- TIWS_Praktikum_3.pl
+++ TIWS_Praktikum_3.pl
@@ -1,7 +1,7 @@
 % Autor:
 % Datum: 14.11.2013
 
-%Aufgabe 1:
+%Aufgabe 1: (Binärbäume und Listen)
 %präorder(Xb,Ys) : Xb ist ein Baum, Ys die Liste der Baumknoten in präorder.
 präorder([],[]).
 präorder([X|[Lb,Rb]],Ys) :- präorder(Lb,H1s), präorder(Rb,H2s), append([X],H1s,H3s), append(H3s,H2s,Ys).
@@ -23,10 +23,15 @@ tiefe([X|[Lb,Rb]],Ys) :- tiefe(Lb,H1s), tiefe(Rb,H2s), append(H1s,[X],H3s), appe
 
 
 %roots(Xbs,Ys)
+roots([],[]) :-!.
+roots([Xb|Ybs],Es) :- root(Xb,X1), roots(Ybs,H1s), append([X1],H1s,Es).
+
+%Q roots([[1, [2,[],[]] ,[]],[3, [4,[],[]] ,[]],[23, [32,[],[]] ,[]]],Zs).
+%Q roots([[5, [3,[2,[],[]],[4,[],[]]] ,[7,[6,[],[]],[8,[],[]]]],[5, [3,[2,[],[]],[4,[],[]]] ,[7,[6,[],[]],[8,[],[]]]],[5, [3,[2,[],[]],[4,[],[]]] ,[7,[6,[],[]],[8,[],[]]]]],Zs).
 
 %Hilfskonstrukte
 %bintree(Xb) : Xb ist ein Binärbaum
-bintree([]).
+bintree([]) :-!.
 bintree([X|[Lb,Rb]]) :- bintree(Lb), bintree(Rb).
 %bintree(list(X,list(R))
 
@@ -42,6 +47,23 @@ right([X|[Lb,Rb]],Rb).
 dosort([],[]).
 swap(X,Y,Rs)  .
 
+
+%Sonderufgabe (außer Konkrruenz)
+%breite(Xb,Ys).
+%breite([X|[Lb,Rb]],Ys) :- roots(Lb,H1s), roots(Rb,H2s), breite(), append(H1s,H2s,Ys).
+
+
+%Aufgabe 2: (Türme von Hanoi)
+%tof(X,Ys).
+tof(N,Ys) :- move(N,1,3,2,Ys).
+
+%move(N,A,B,C,Ys)
+move(0,A,B,C,[]) :-!.
+%move(1,A,B,C,[von, A, nach, B]) :-!.
+move(N,A,B,C,Ys) :- Hn is N-1, move(Hn,A,C,B,H1s), move(Hn,C,B,A,H2s), append(H1s,[von, A, nach, B],H3s), append(H3s,H2s,Ys).
+
+
+%Aufgabe 3: (Quicksort)
 %quicksort : erstelle linke liste mit allen die <= als Pivotelement und rechte mit >.
 qsort([],[]) :-!.
 qsort([X|Xs],Ys) :- getPivot([X|Xs],Pivot,XPs), partlist(XPs,Pivot,Ls,Rs), qsort(Ls,E1s), qsort(Rs,E2s),  append(E1s,[Pivot],E3s), append(E3s,E2s,Ys).
@@ -58,37 +80,3 @@ getPivot([X|Xs],X,Xs).
 %rightlist([3,2,4],3,[4]).
 %leftlist([2,3],2,[2]).
 %rightlist([2,3],2,[3]).
-
-
-
-
-
-
-
-
-
-
-
-
-
-tof(X,Ys) .
-
-hanoi(1,A,B,_,[zug(A,B)]).
-hanoi(N,A,B,C,Z) :- M is N-1, hanoi(M,A,C,B,Z1), hanoi(M,C,B,A,Z2), append(Z1,[zug(A,B)|Z2],Z).
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-han(X,Ys) :- zug(X,A,B,C,Ys).
-zug(1,A,B,C,[von,a,nach,c]).
-zug(N,A,B,C,Es) :- M is N-1, zug(M,A,B,C,Es).
